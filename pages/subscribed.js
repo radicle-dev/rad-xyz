1550328c05b3dd215aa296357d86729cfee33121
Julien Donck <donckjulien@gmail.com>
2019-12-16 11:39:11 +0100

    subscription confirmation page

--- pages/subscribed.js
+++ pages/subscribed.js
@@ -3,28 +3,28 @@ import Link from 'next/link'
 import 'styles/style.scss'
 export default function Index() {
   return (
-      <section id="subscribed">
-        <div className="container">
-          <h1>
+    <section id="subscribed">
+      <div className="container">
+        <h1>
           Thank you!
-          </h1>
-          <section id="flowers-success">
-            <pre><code>                _(_)_                     _(_)_                     _(_)_     </code></pre>
-            <pre><code>    @@@@       (_)@(_)        @@@@       (_)@(_)        @@@@       (_)@(_)    </code></pre>
-            <pre><code>   @@()@@ wWWWw  (_)\        @@()@@ wWWWw  (_)\        @@()@@ wWWWw  (_)\     </code></pre>
-            <pre><code>    @@@@  (___)     \|/       @@@@  (___)     \|/       @@@@  (___)     \|/   </code></pre>
-            <pre><code>     /      Y       \|         /      Y       \|         /      Y       \|    </code></pre>
-            <pre><code>  \ |     \ |/       | /    \ |     \ |/       | /    \ |     \ |/       | /  </code></pre>
-            <pre><code>  \\|//   \\|///  \\\|//    \\|//   \\|///  \\\|//    \\|//   \\|///  \\\|//  </code></pre>
-            <pre><code>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^</code></pre>
-          </section>
-          <br/>
-          <Link href="/">
-            <button>
+        </h1>
+        <section id="flowers-success">
+          <pre><code>                _(_)_                     _(_)_                     _(_)_     </code></pre>
+          <pre><code>    @@@@       (_)@(_)        @@@@       (_)@(_)        @@@@       (_)@(_)    </code></pre>
+          <pre><code>   @@()@@ wWWWw  (_)\        @@()@@ wWWWw  (_)\        @@()@@ wWWWw  (_)\     </code></pre>
+          <pre><code>    @@@@  (___)     \|/       @@@@  (___)     \|/       @@@@  (___)     \|/   </code></pre>
+          <pre><code>     /      Y       \|         /      Y       \|         /      Y       \|    </code></pre>
+          <pre><code>  \ |     \ |/       | /    \ |     \ |/       | /    \ |     \ |/       | /  </code></pre>
+          <pre><code>  \\|//   \\|///  \\\|//    \\|//   \\|///  \\\|//    \\|//   \\|///  \\\|//  </code></pre>
+          <pre><code>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^</code></pre>
+        </section>
+        <br/>
+        <Link href="/">
+          <button>
             &larr; Back home
-            </button>
-          </Link>
-        </div>
-      </section>
+          </button>
+        </Link>
+      </div>
+    </section>
   )
 }
