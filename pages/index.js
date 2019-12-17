7a818b874b8bd69dbc9bb7a3a81c8fd463b4637c
Alexis Sellier <alexis@monadic.xyz>
2019-12-17 18:19:11 +0100

    Update collaboration model

--- pages/index.js
+++ pages/index.js
@@ -54,8 +54,11 @@ export default function Index() {
               </section>
             </div>
             <p>
-              Peer-to-peer networking offers a new design space for bazaar-style development. <br/>
-              issues & prs locally /
+              Peer-to-peer networking offers a new design space for <a href="http://www.catb.org/~esr/writings/cathedral-bazaar/cathedral-bazaar/">bazaar-style</a> development.
+              Radicle was built to offer a convenient collaboration workflow without intermedaries or central servers. Issues, patches and code review items can be
+              shared seamlessly between peers and interacted with on the user's machine, just like one interacts with a <code>git</code> repository. Login systems are replaced by public key cryptography, hosted issue trackers are replaced by peer
+              replication and locally-hosted software, and the idea of a code repository that one pushes to is replaced by a multi-upstream model familiar
+              to the open source hackers of the 90s and early 2000s&mdash;enhanced with a new protocol for peer collaboration.
             </p>
             <div className="left-box">
               <h2>How it works</h2>
